6fe7b2e0e7cb5919f52ec4a66cff1a18ea248dfa
Vai Srivastava <vaisriv@icloud.com>
2026-03-04 17:49:04 -0500

    chore(nix)!: bump nixpkgs version to release-25.11, move from alejandra to nixfmt for nix formatter, and move nix dir from `./.nix` to `./nix`

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     inputs = {
         # nixpkgs
-        nixpkgs.url = "github:nixos/nixpkgs";
+        nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
         # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
         # flake tools (thanks numtide)
@@ -30,6 +30,6 @@
     outputs = inputs:
         inputs.blueprint {
             inherit inputs;
-            prefix = "./.nix/";
+            prefix = "./nix/";
         };
 }
--- nix/formatter.nix
+++ nix/formatter.nix
@@ -7,14 +7,11 @@ inputs.treefmt-nix.lib.mkWrapper pkgs {
     projectRootFile = "flake.nix";
 
     # nix
-    programs.deadnix.enable = true;
-    programs.alejandra.enable = true;
-    settings.formatter = {
-        alejandra = {
-            options = [
-                "--experimental-config"
-                "./.nix/fmt/alejandra.toml"
-            ];
+    programs = {
+        deadnix.enable = true;
+        nixfmt = {
+            enable = true;
+            indent = 4;
         };
     };
 
